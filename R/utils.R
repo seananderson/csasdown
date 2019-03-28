80a16a78a750d5842cfa45d3304f255151185fdc
Chris Grandin <cgrandin@shaw.ca>
2019-03-28 11:40:39 -0700

    Make list sub elements have same space beteen as the main list elements.

--- R/utils.R
+++ R/utils.R
@@ -205,7 +205,7 @@ fix_envs <- function(x) {
   x <- gsub("itemize\\}", "resdoclist\\}", x)
   x <- gsub("enumerate\\}", "resdoclist\\}", x)
 
-  x <- gsub("^\\\\tightlist$", "", x)
+  x <- gsub("^.*\\\\tightlist$", "", x)
   # Non-breaking spaces:
   x <- gsub(" \\\\ref\\{", "~\\\\ref\\{", x)
 
