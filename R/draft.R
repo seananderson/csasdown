61df6d0a3cacc5452a6ac81673c4943323c197be
Sean Anderson <sean@seananderson.ca>
2019-03-29 09:28:31 -0700

    Fixed documentation

--- R/draft.R
+++ R/draft.R
@@ -5,6 +5,7 @@
 #' @param type The type of document to start.
 #' @param create_dir `TRUE` to create a new directory for the document.
 #' @param edit `TRUE` to edit the template immediately.
+#' @param ... Other arguments to pass to [rmarkdown::draft()].
 #'
 #' @details This is a light wrapper around [rmarkdown::draft()]. Consult that
 #'   function for further details.
