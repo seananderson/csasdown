b7e01c4957cfb4838deaf8f1906af8902ad807c4
Chris Grandin <cgrandin@shaw.ca>
2019-06-06 12:45:24 -0700

    Implement landscape tables
    Changed csas_table() to allow for landscape tables. Added an example to both resdoc and techreport examples.

--- R/tables.R
+++ R/tables.R
@@ -8,14 +8,44 @@
 #' @param booktabs As defined by [knitr::kable()].
 #' @param linesep As defined by [knitr::kable()].
 #' @param longtable As defined by [knitr::kable()].
+#' @param font_size Font size in pts.
+#' @param lanscape Make this table in landscape orientation?
+#' @param repeat_header If landscape, repeat the header on subsequent pages?
 #' @param ... Other arguments to pass to [knitr::kable()].
 #'
 #' @examples
 #' csas_table(head(iris))
 #' @export
-
-csas_table <- function(x, format = "pandoc", booktabs = TRUE, linesep = "",
-  longtable = TRUE, ...)  {
-  knitr::kable(x = x, format = format, booktabs = booktabs, linesep = linesep,
-    longtable = longtable, ...)
+csas_table <- function(x,
+                       format = "pandoc",
+                       booktabs = TRUE,
+                       linesep = "",
+                       longtable = TRUE,
+                       font_size = 10,
+                       landscape = FALSE,
+                       repeat_header = TRUE,
+                       ...){
+  k <- knitr::kable(x = x,
+                    format = format,
+                    booktabs = booktabs,
+                    linesep = linesep,
+                    longtable = longtable,
+                    ...)
+  if(landscape){
+    k <- k %>%
+      kableExtra::landscape()
+    if(repeat_header){
+      k <- k %>%
+        kableExtra::kable_styling(font_size = font_size,
+                                  latex_options = "repeat_header",
+                                  repeat_header_text = "",
+                                  repeat_header_method = "replace")
+    }else{
+      k <- k %>%
+        kableExtra::kable_styling(font_size = font_size)
+    }
+    k <- k %>%
+      sub("\\caption\\[\\]\\{\\}", "\\caption*{}", .)
+  }
+  k
 }
