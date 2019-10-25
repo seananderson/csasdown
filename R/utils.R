43d0c7c0df783f10294e4d4e02380ad80a69782e
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2019-10-25 13:34:44 -0700

    Options to english citation

--- R/utils.R
+++ R/utils.R
@@ -361,7 +361,7 @@ fix_envs <- function(x,
     cite_loc <- grep(
       pattern="Correct citation for this publication", x=x )
     cite_text <- x[cite_loc]
-    cite_text_new <- "\\MakeAvailable{Correct citation (until published):}{\\CiteEng{}}{}{}"
+    cite_text_new <- "\\MakeAvailable{Correct citation (until published):}{\\CiteEng{In press}}{}{}"
     x[cite_loc] <- cite_text_new
   }
 
