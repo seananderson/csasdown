7590985d69128ef81c9c19c68548a1f4fac036d3
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2020-01-21 10:13:17 -0800

    Add regional addresses

--- R/utils.R
+++ R/utils.R
@@ -428,13 +428,13 @@ get_contact_info <- function( region="National Capital Region", isFr=FALSE ) {
   # Create a table with region name, region name if french, and email address
   dat <- tibble::tribble(
     ~Region, ~RegionFr, ~Email, ~Phone, ~Address,
-    "Central and Arctic Region", "R\'{e}gion du Centre et de l'Arctique", "xcna-csa-cas@dfo-mpo.gc.ca", "(204) 983-5232", "",
-    "Gulf Region", "R\'{e}gion du Golfe", "Gerald.Chaput@dfo-mpo.gc.ca", "(506) 851-2022", "",
-    "Maritimes Region", "R\'{e}gion des Maritimes", "XMARMRAP@dfo-mpo.gc.ca", "(902) 426-3246", "",
-    "National Capital Region", "R\'{e}egion de la capitale nationale", "csas-sccs@dfo-mpo.gc.ca", "(613) 990-0194", "",
-    "Newfoundland and Labrador Region", "R\'{e}gion de Terre-Neuve et Labrador", "DFONLCentreforScienceAdvice@dfo-mpo.gc.ca", "(709) 772-8892", "",
-    "Pacific Region", "R\'{e}gion du Pacifique", "csap@dfo-mpo.gc.ca", "(250) 756-7088", "",
-    "Quebec Region", "R\'{e}gion du Qu\'{e}bec", "bras@dfo-mpo.gc.ca", "(418) 775-0825", "" )
+    "Central and Arctic Region", "R\'{e}gion du Centre et de l'Arctique", "xcna-csa-cas@dfo-mpo.gc.ca", "(204) 983-5232", "501 University Cres.\\Winnipeg, MB, R3T 2N6",
+    "Gulf Region", "R\'{e}gion du Golfe", "Gerald.Chaput@dfo-mpo.gc.ca", "(506) 851-2022", "343 Universit\'{e} Ave.\\Moncton, NB, E1C 9B6",
+    "Maritimes Region", "R\'{e}gion des Maritimes", "XMARMRAP@dfo-mpo.gc.ca", "(902) 426-3246", "1 Challenger Dr.\\Dartmouth, NS, B2Y 4A2",
+    "National Capital Region", "R\'{e}egion de la capitale nationale", "csas-sccs@dfo-mpo.gc.ca", "(613) 990-0194", "200 Kent St.\\Ottawa, ON, K1A 0E6",
+    "Newfoundland and Labrador Region", "R\'{e}gion de Terre-Neuve et Labrador", "DFONLCentreforScienceAdvice@dfo-mpo.gc.ca", "(709) 772-8892", "P.O. Box 5667 St. John's, NL, A1C 5X1",
+    "Pacific Region", "R\'{e}gion du Pacifique", "csap@dfo-mpo.gc.ca", "(250) 756-7088", "3190 Hammond Bay Rd.\\Nanaimo, BC, V9T 6N7",
+    "Quebec Region", "R\'{e}gion du Qu\'{e}bec", "bras@dfo-mpo.gc.ca", "(418) 775-0825", "850 route de la Mer, P.O. Box 1000\\Mont-Joli, QC, G5H 3Z4" )
   # If french
   if( isFr ) {
     # Get index for region (row)
