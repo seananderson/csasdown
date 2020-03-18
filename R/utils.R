eeeda95a6c00dea7853a7699fee663178dc3ab55
Sean Anderson <sean@seananderson.ca>
2020-03-18 12:50:57 -0700

    Simplify bug fix

--- R/utils.R
+++ R/utils.R
@@ -475,19 +475,13 @@ fix_envs <- function(x,
 inject_refstepcounters <- function(x) {
   chpts <- grep("^\\\\starredchapter\\{", x)
   for (i in chpts) {
-    if (grepl("hypertarget", x[i-1])) {
-      x <- c(
-        x[seq(1, i - 3)],
-        paste0(x[i - 2], "\n\n\\clearpage\n\n\\refstepcounter{chapter}"),
-        x[seq(i - 1, length(x))]
-      )
-    } else { # otherwise; in very rare setups(?)
-      x <- c(
-        x[seq(1, i - 2)],
-        paste0(x[i - 1], "\n\n\\clearpage\n\n\\refstepcounter{chapter}"),
-        x[seq(i, length(x))]
-      )
-    }
+    # in very rare setups hypertarget doesn't appear(?):
+    .i <- if (grepl("hypertarget", x[i-1])) i else i - 1
+    x <- c(
+      x[seq(1, .i - 3)],
+      paste0(x[.i - 2], "\n\n\\clearpage\n\n\\refstepcounter{chapter}"),
+      x[seq(.i - 1, length(x))]
+    )
   }
   x
 }
