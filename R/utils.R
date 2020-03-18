9f5bec461e034077a3f74d49ba1a8b1b6f0e4a3d
Sean Anderson <sean@seananderson.ca>
2020-03-18 13:30:03 -0700

    Add --default-image-extension

--- R/utils.R
+++ R/utils.R
@@ -37,7 +37,7 @@ resdoc_pdf <- function(toc = TRUE, toc_depth = 3, highlight = "default",
     toc_depth = toc_depth,
     highlight = highlight,
     keep_tex = TRUE,
-    pandoc_args = c("--top-level-division=chapter", "--wrap=none"),
+    pandoc_args = c("--top-level-division=chapter", "--wrap=none", "--default-image-extension=png"),
     latex_engine = latex_engine,
     ...
   )
