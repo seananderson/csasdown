a3364785d1d6533209d6b5e165ed248b1f133c4a
Sean Anderson <sean@seananderson.ca>
2021-01-13 10:58:31 -0800

    Fix #165

--- R/csasdown.R
+++ R/csasdown.R
@@ -1,4 +1,10 @@
-#' csasdown: A package for creating CSAS Research Document with R Markdown / bookdown
+#' csasdown: A package for creating CSAS Research Document with R Markdown/bookdown
+#'
+#' Uses the 'bookdown' R package to generate CSAS (Canadian Science Advisory
+#' Secretariat) Research Documents, Science Responses, and Technical Reports in
+#' PDF or Word format. The package is based on Chester Ismay's thesisdown
+#' package and Ben Marwick's huskydown package.
+#'
 #' @docType package
 #' @name csasdown
 NULL
