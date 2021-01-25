69804729327b052b4c7b33b16ccbb8c934a809da
Sean Anderson <sean@seananderson.ca>
2021-01-25 11:02:56 -0800

    Comment out unnecessary tests

--- tests/testthat/test-check-prerequisites.R
+++ tests/testthat/test-check-prerequisites.R
@@ -1,29 +1,27 @@
-context("Check for prerequisites")
-
-testing_path <- tempdir()
-
-if(identical(.Platform$OS.type, "windows")){
-  x <- system("where xelatex", intern = TRUE)
-}else{
-  x <- system("which xelatex", intern = TRUE)
-}
-
-if(!grepl("latex", x)) {
-  if (!require(tinytex)){
-    install.packages("tinytex")
-  }
-  if(!tinytex:::is_tinytex()){
-    tinytex::install_tinytex(force = TRUE)
-  }
-  test_that("LaTeX is installed", {
-    expect_true(tinytex:::is_tinytex())
-  })
-}
-
-template_files <- list.files(system.file("rmarkdown",
-                                         package = "csasdown"),
-                             recursive = TRUE)
-
-test_that("csasdown template files are present", {
-  expect_true(length(template_files) > 10)
-})
+# testing_path <- tempdir()
+#
+# if(identical(.Platform$OS.type, "windows")){
+#   x <- system("where xelatex", intern = TRUE)
+# }else{
+#   x <- system("which xelatex", intern = TRUE)
+# }
+#
+# if(!grepl("latex", x)) {
+#   if (!require(tinytex)){
+#     install.packages("tinytex")
+#   }
+#   if(!tinytex:::is_tinytex()){
+#     tinytex::install_tinytex(force = TRUE)
+#   }
+#   test_that("LaTeX is installed", {
+#     expect_true(tinytex:::is_tinytex())
+#   })
+# }
+#
+# template_files <- list.files(system.file("rmarkdown",
+#                                          package = "csasdown"),
+#                              recursive = TRUE)
+#
+# test_that("csasdown template files are present", {
+#   expect_true(length(template_files) > 10)
+# })
