6a24a7d389ed5a7c0abb424e4e8a4df4815e55a1
Chris Grandin <chris.grandin@gmail.com>
2021-06-01 10:39:28 -0700

    Add docker files

---
+++ docker/install_packages.R
@@ -0,0 +1,10 @@
+install.packages("remotes")
+
+# Install knitr and xaringan presentation package
+remotes::install_github("yihui/xfun")
+remotes::install_github("yihui/knitr")
+remotes::install_github("yihui/xaringan")
+remotes::install_github("gadenbuie/xaringanthemer")
+
+# Install csasdown and dependencies
+remotes::install_github("pbs-assess/csasdown")
