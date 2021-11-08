f2b4c8607d978ad6bbe732d5d12e53e6e1ba6568
Chris Grandin <chris.grandin@gmail.com>
2021-11-08 01:53:53 -0800

    Add one last depth to the Appendix referencing e.g A.1.1.1 works * This will finish #179 for good * I am getting carried away now

--- R/utils.R
+++ R/utils.R
@@ -700,11 +700,11 @@ add_appendix_subsection_refs <- function(x){
     # one element for each appendix
 
     # Apply mods to the appendix chunks
-    for(k in seq_along(appendix_chunks)){
-      appsection_inds <- grep("^\\\\appsection\\{", appendix_chunks[[k]])
+    for(h in seq_along(appendix_chunks)){
+      appsection_inds <- grep("^\\\\appsection\\{", appendix_chunks[[h]])
       if(length(appsection_inds)){
         # Strip appendix header away and call function on the rest
-        app_chunk <- appendix_chunks[[k]]
+        app_chunk <- appendix_chunks[[h]]
         app_header <- app_chunk[1:(appsection_inds[1] - 2)]
         app_chunk <- app_chunk[(appsection_inds[1] - 1):length(app_chunk)]
         app_chunk_inds <- grep("^\\\\appsection\\{", app_chunk)
@@ -735,6 +735,7 @@ add_appendix_subsection_refs <- function(x){
             sec_chunk <- sec_chunk[(subsection_inds[1] - 1):length(sec_chunk)]
             sec_chunk_inds <- grep("^\\\\subsection\\{", sec_chunk)
             subsec_chunks <- list()
+            subsec_header <- list()
             for(j in seq_along(sec_chunk_inds)){
               if(j == length(sec_chunk_inds)){
                 subsec_chunks[[j]] <- sec_chunk[(sec_chunk_inds[j] - 1):length(sec_chunk)]
@@ -749,16 +750,58 @@ add_appendix_subsection_refs <- function(x){
                 subsec_chunks[[j]][1] <- tmp_subsection
                 subsec_chunks[[j]][2] <- tmp_label
               }
+              # Iterate through each section chunk and create a list for the subsection chunks
+              subsubsection_inds <- grep("^\\\\subsubsection\\{", subsec_chunks[[j]])
+              if(length(subsubsection_inds)){
+                subsec_chunk <- subsec_chunks[[j]]
+                subsec_header[[j]] <- subsec_chunk[1:(subsubsection_inds[1] - 2)]
+                subsec_chunk <- subsec_chunk[(subsubsection_inds[1] - 1):length(subsec_chunk)]
+                subsec_chunk_inds <- grep("^\\\\subsubsection\\{", subsec_chunk)
+                subsubsec_chunks <- list()
+                for(k in seq_along(subsec_chunk_inds)){
+                  if(k == length(subsec_chunk_inds)){
+                    subsubsec_chunks[[k]] <- subsec_chunk[(subsec_chunk_inds[k] - 1):length(subsec_chunk)]
+                  }else{
+                    subsubsec_chunks[[k]] <- subsec_chunk[(subsec_chunk_inds[k] - 1):(subsec_chunk_inds[k + 1] - 2)]
+                  }
+                  if(!length(grep("^\\\\hypertarget\\{", subsubsec_chunks[[k]][1]))){
+                    # An auto-generated label was not added (using manually-added label) so switching the
+                    # label and subsection is necessary
+                    tmp_sublabel <- subsubsec_chunks[[k]][1]
+                    tmp_subsubsection <- subsubsec_chunks[[k]][2]
+                    subsubsec_chunks[[k]][1] <- tmp_subsubsection
+                    subsubsec_chunks[[k]][2] <- tmp_sublabel
+                  }
+                }
+                subsubsec_chunks <- unlist(subsubsec_chunks)
+                counter_lines <- c(paste0("\\newcounter{appendix_",
+                                          h,
+                                          "_appsection_",
+                                          i,
+                                          "_subsection_",
+                                          j,
+                                          "_counter}"),
+                                   paste0("\\refstepcounter{appendix_",
+                                          h,
+                                          "_appsection_",
+                                          i,
+                                          "_subsection_",
+                                          j,
+                                          "_counter}"))
+                subsubsec_chunks <- c(counter_lines, subsubsec_chunks)
+                names(subsubsec_chunks) <- NULL
+                subsec_chunks[[j]] <- c(subsec_header[[j]], subsubsec_chunks)
+              }
             }
             subsec_chunks <- unlist(subsec_chunks)
             counter_lines <- c(paste0("\\newcounter{appendix_",
-                                      k,
-                                      "_subsection_",
+                                      h,
+                                      "_appsection_",
                                       i,
                                       "_counter}"),
                                paste0("\\refstepcounter{appendix_",
-                                      k,
-                                      "_subsection_",
+                                      h,
+                                      "_appsection_",
                                       i,
                                       "_counter}"))
             subsec_chunks <- c(counter_lines, subsec_chunks)
@@ -769,21 +812,20 @@ add_appendix_subsection_refs <- function(x){
         sec_chunks <- unlist(sec_chunks)
         sec_header <- NULL
         counter_lines <- c(paste0("\\newcounter{appendix_",
-                                  k,
-                                  "_appsection_counter}"),
+                                  h,
+                                  "_counter}"),
                            paste0("\\refstepcounter{appendix_",
-                                  k,
-                                  "_appsection_counter}"))
+                                  h,
+                                  "_counter}"))
         sec_chunks <- c(counter_lines, sec_chunks)
         names(sec_chunks) <- NULL
-        appendix_chunks[[k]] <- c(app_header, sec_chunks)
+        appendix_chunks[[h]] <- c(app_header, sec_chunks)
       }
     }
-    appendix_chunks <- unlist(appendix_chunks)
-    names(appendix_chunks) <- NULL
-    x <- c(pre_starred_x, appendix_chunks)
   }
-
+  appendix_chunks <- unlist(appendix_chunks)
+  names(appendix_chunks) <- NULL
+  x <- c(pre_starred_x, appendix_chunks)
   x
 }
 
