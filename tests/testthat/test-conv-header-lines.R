28fefc5f94db40d77655912fa1bcb58ad5ed3cde
Chris Grandin <chris.grandin@gmail.com>
2022-06-06 00:16:45 -0700

    Make conv_rmarkdown_header_lines() deal with spaces * And all the following trailing whitespace * Add tests and fix old tests

--- tests/testthat/test-conv-header-lines.R
+++ tests/testthat/test-conv-header-lines.R
@@ -4,30 +4,40 @@ test_that("Conversion of header lines in Rmd works correctly", {
   tmp <- conv_header_lines(chunk)
   expect_null(tmp[[1]])
   expect_null(tmp[[2]])
+  # ---------------------------------------------------------------------------
 
   chunk <- "# Header 1"
   tmp <- conv_header_lines(chunk)
   expect_identical(tmp[[1]], "# Header 1")
   expect_null(tmp[[2]])
+  # ---------------------------------------------------------------------------
 
-  chunk <- c("# Header 1", "## Subheader 1", "## Subheader 2", "### Subsubheader 1")
+  chunk <- c("# Header 1", "## Subheader 1", "## Subheader 2",
+             "### Subsubheader 1")
   tmp <- conv_header_lines(chunk)
   expect_identical(tmp[[1]], chunk)
   expect_null(tmp[[2]])
+  # ---------------------------------------------------------------------------
 
   chunk <- c("# Header 1", "", "",
              "## Subheader 1", "", "",
              "## Subheader 2", "### Subsubheader 1")
-  tmp <- conv_header_lines(chunk)
-  expect_identical(tmp[[1]], c("# Header 1", "## Subheader 1", "## Subheader 2", "### Subsubheader 1"))
+  expect_warning(tmp <- conv_header_lines(chunk))
+  expect_identical(tmp[[1]], c("# Header 1", "## Subheader 1",
+                               "## Subheader 2", "### Subsubheader 1"))
   expect_null(tmp[[2]])
+  # ---------------------------------------------------------------------------
 
   chunk <- c("# Header 1", "", "",
              "## Subheader 1",
-             "## Subheader 2", "### Subsubheader 1", "", "")
-  tmp <- conv_header_lines(chunk)
-  expect_identical(tmp[[1]], c("# Header 1", "## Subheader 1", "## Subheader 2", "### Subsubheader 1"))
-  expect_identical(tmp[[2]], "")
+             "## Subheader 2",
+             "### Subsubheader 1", "", "")
+  expect_warning(tmp <- conv_header_lines(chunk))
+  expect_identical(tmp[[1]], c("# Header 1", "## Subheader 1",
+                               "## Subheader 2", "### Subsubheader 1",
+                               "", "\\\\", ""))
+  expect_null(tmp[[2]])
+  # ---------------------------------------------------------------------------
 
   chunk <- c("", "", "# Header 1", "", "",
              "## Subheader 1",
@@ -35,5 +45,64 @@ test_that("Conversion of header lines in Rmd works correctly", {
   tmp <- conv_header_lines(chunk)
   expect_null(tmp[[1]])
   expect_identical(tmp[[2]], chunk)
+  # ---------------------------------------------------------------------------
 
+  chunk <- c("# H1", "")
+  tmp <- conv_header_lines(chunk)
+  expect_identical(tmp[[1]], c("# H1", "", "\\\\ \\\\", ""))
+  expect_null(tmp[[2]])
+  # ---------------------------------------------------------------------------
+
+  chunk <- c("# H1", "", "", "", "", "Normal text", "", "", "", "", "", "# H2")
+  tmp <- conv_header_lines(chunk)
+  expect_identical(tmp[[1]], c("# H1", "", "\\\\", "\\\\", "\\\\", ""))
+  expect_identical(tmp[[2]], c("Normal text", "", "", "", "", "", "# H2"))
+  # ---------------------------------------------------------------------------
+
+  chunk <- c("# H1",
+             "# H2",
+             "# H3")
+  tmp <- conv_header_lines(chunk)
+  expect_identical(tmp[[1]], c("# H1", "# H2", "# H3"))
+  expect_null(tmp[[2]])
+  # ---------------------------------------------------------------------------
+
+  chunk <- c("# H1", "",
+             "# H2",
+             "# H3")
+  expect_warning(tmp <- conv_header_lines(chunk))
+  expect_identical(tmp[[1]], c("# H1", "# H2", "# H3"))
+  expect_null(tmp[[2]])
+  # ---------------------------------------------------------------------------
+
+  chunk <- c("# H1",
+             "# H2", "",
+             "# H3")
+  expect_warning(tmp <- conv_header_lines(chunk))
+  expect_identical(tmp[[1]], c("# H1", "# H2", "# H3"))
+  expect_null(tmp[[2]])
+  # ---------------------------------------------------------------------------
+
+  chunk <- c("# H1", "",
+             "# H2", "",
+             "# H3")
+  expect_warning(tmp <- conv_header_lines(chunk))
+  expect_identical(tmp[[1]], c("# H1", "# H2", "# H3"))
+  expect_null(tmp[[2]])
+  # ---------------------------------------------------------------------------
+
+  chunk <- c("# H1", "",
+             "# H2", "",
+             "# H3", "")
+  expect_warning(tmp <- conv_header_lines(chunk))
+  expect_identical(tmp[[1]], c("# H1", "# H2", "# H3", "", "\\\\ \\\\", ""))
+  expect_null(tmp[[2]])
+  # ---------------------------------------------------------------------------
+
+  chunk <- c("# H1",
+             "# H2",
+             "# H3", "")
+  tmp <- conv_header_lines(chunk)
+  expect_identical(tmp[[1]], c("# H1", "# H2", "# H3", "", "\\\\ \\\\", ""))
+  expect_null(tmp[[2]])
 })
