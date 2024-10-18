4d387244c77a01045535bbbaf53af4ae2f80fc66
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2024-10-18 10:19:58 -0230

    Disable default bolding of table and figure  caption prefex

--- R/resdoc-word.R
+++ R/resdoc-word.R
@@ -49,7 +49,10 @@ resdoc_word2 <- function(...) {
                                      tables = list(
                                        style = "Compact", layout = "autofit", width = 1,
                                        caption = list(
-                                         style = "Table Caption", pre = "Table ", sep = ". "),
+                                         style = "Table Caption",
+                                         pre = "Table", sep = ". ",
+                                         fp_text = officer::fp_text_lite(bold = FALSE)
+                                         ),
                                        conditional = list(
                                          first_row = TRUE, first_column = FALSE, last_row = FALSE,
                                          last_column = FALSE, no_hband = FALSE, no_vband = TRUE
@@ -60,7 +63,8 @@ resdoc_word2 <- function(...) {
                                        align = "center",
                                        caption = list(
                                          style = "Caption - Figure",
-                                         pre = "Figure ", sep = ". "
+                                         pre = "Figure ", sep = ". ",
+                                         fp_text = officer::fp_text_lite(bold = FALSE)
                                        )
                                      ),
                                      reference_docx = system.file("csas-docx",
